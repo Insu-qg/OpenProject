8dece35424d123fad9aa272f65d67d8a7bc34c5d
Insu-qg <quent.grange@gmail.com>
2025-02-13 10:22:44 +0100

    ✨ Update Vite configuration to specify build output directory and minification settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // le dossier de sortie de l'app
+    minify: 'esbuild', // pour une minification efficace
+    chunkSizeWarningLimit: 1000, // augmenter ou diminuer la limite si nécessaire
+  }
 })
